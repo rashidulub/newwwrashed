56be3c2e69ee19145cc58a34ec97b056749644f6
akil-tajwar <akiltajwar@gmail.com>
2023-08-09 23:37:30 +0600

    banner style, space between components

--- src/Components/home/MoreInfo/MoreInfo.jsx
+++ src/Components/home/MoreInfo/MoreInfo.jsx
@@ -6,10 +6,10 @@ import gellary4 from "@/asstes/gellary4.jpg";
 
 const MoreInfo = () => {
 	return (
-		<div className="grid grid-cols-2 px-[1.3rem] gap-4 mt-20 w-full">
+		<div className="flex px-[1.3rem] gap-4 mt-20 w-full py-20">
 			<div className="w-fit grid grid-cols-2 gap-4">
 				<Image
-					width={250}
+					width={500}
 					height={100}
 					alt="carGallary"
 					className=" rounded shadow-sm min-h-12 dark:bg-gray-500 "
@@ -17,20 +17,20 @@ const MoreInfo = () => {
 				/>
 				<Image
 					alt="foodGallary"
-					width={250}
+					width={500}
 					height={100}
 					className=" rounded shadow-sm min-h-48 dark:bg-gray-500 "
 					src={gellary3}
 				/>
 				<Image
 					alt="carGallary"
-					width={250}
+					width={500}
 					height={100}
 					className="rounded shadow-sm min-h-48 dark:bg-gray-500 "
 					src={gellary2}
 				/>
 				<Image
-					width={250}
+					width={500}
 					height={100}
 					alt="foodGallary"
 					className="rounded shadow-sm min-h-48 dark:bg-gray-500 "
@@ -38,7 +38,7 @@ const MoreInfo = () => {
 				/>
 			</div>
 			<div>
-				<h1 className="text-4xl text-[#0083db] font-medium">Learn more</h1>
+				<h1 className="text-4xl text-[#0083db] font-semibold">Learn more</h1>
 				<p className="mt-3">
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem modi
 					minus veniam. Maiores, obcaecati ipsa debitis quisquam odit neque.
