eeae542aa78881ce33d7ebfb988656ddacaf96b5
RaW <akram.iiuc.ctg@gmail.com>
2023-08-26 20:36:17 +0600

    Assignment and Notice Component Updated

--- src/app/api/notice/route.js
+++ src/app/api/notice/route.js
@@ -3,50 +3,25 @@ import bdConnect from "@/utils/dbConnect";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const { notice_id, title, description, due_date, For, topic } = await request.json();
+  const { notice_id, title, description, due_date, For, topic} = await request.json();
 
-    await bdConnect();
-    try {
-        const notices = await Notices.create({
-            notice_id, title, description, due_date, For, topic
-        });
-        return NextResponse.json(notices);
-    } catch (error) {
-        return NextResponse.json({
-            meg: "data not added",
-        });
-    }
+  await bdConnect();
+  try {
+    const notice = await Notices.create({
+        notice_id, title, description, due_date, For, topic
+    });
+    return NextResponse.json(notice);
+  } catch (error) {
+    return NextResponse.json({
+      meg: "data not added",
+    });
+  }
 }
 
-
-
-// export async function GET() {
-//     let data = [];
-//     let success = true;
-
-//     try {
-//         await bdConnect();
-//         data = await Notices.find();
-
-//     } catch (error) {
-//         data = { result: "error" }
-//         success: false;
-
-//     }
-//     return NextResponse.json({ result: data, success })
-// }
-
-
-
 export async function GET() {
-    await bdConnect();
-    try {
-        const notices = await Notices.find({}); // Retrieve all courses
-        return NextResponse.json({ notices }, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({
-            message: "Failed to retrieve notices",
-            status: 500,
-        });
-    }
+  await bdConnect();
+  try {
+    const data = await Notices.find();
+    return NextResponse.json(data);
+  } catch (error) { }
 }
