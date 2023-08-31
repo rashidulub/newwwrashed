2e0dee99e2daebfd3d83657a123a84be3e567adb
akil-tajwar <akiltajwar@gmail.com>
2023-09-01 01:16:55 +0600

    working on delete account button

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,6 +1,37 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 
 const Settings = () => {
+    const [disabled, setDisabled] = useState(true);
+    const [inputValue, setInputValue] = useState('');
+    const buttonDisable = () => {
+        if (inputValue === 'Delete') {
+            setDisabled(false);
+        }
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    };
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setInputValue(value);
+        setDisabled(value !== 'Delete');
+    };
     return (
         <div className='pt-32 lg:w-3/4 w-11/12 mx-auto px-7 py-10'>
             {/* <h3 className='font-semibold text-3xl text-[#0083db] pb-10'>Setting and Privacy</h3> */}
@@ -60,7 +91,7 @@ const Settings = () => {
                         </div>
                     </div>
                     <div className='grid grid-cols-3 p-3'>
-                        <h6>Which classes do you want to notified from</h6>
+                        <h6>Which classes do you want to get notified from</h6>
                         <p className='text-end'>All classes</p>
                         <div className='text-end'>
                             <button className='bg-[#0083db] text-white px-2 rounded'>Edit</button>
@@ -74,10 +105,10 @@ const Settings = () => {
                     <div className='grid grid-cols-3'>
                         <h6>Delete this account</h6>
                         <div className='flex justify-end'>
-                            <input className='bg-[#e9e9e9] rounded w-1/2 px-2' placeholder='please type "Delete"' type="text" />
+                            <input value={inputValue} onChange={handleInputChange} className='bg-[#e9e9e9] border border-[#b5b5b5] rounded w-1/2 px-2' placeholder='please type "Delete"' type="text" />
                         </div>
                         <div className='text-end'>
-                            <button disabled className='bg-red-200 px-2 rounded'>Yes</button>
+                            <button disabled={disabled} onClick={buttonDisable} className={`px-2 rounded ${disabled ? 'bg-red-200 text-[#a4a4a4]' : 'bg-red-500 text-white'}`}>Yes</button>
                         </div>
                     </div>
                 </div>
