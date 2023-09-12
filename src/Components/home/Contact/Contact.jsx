b2b9cc0459bc956ebf983590ce3521dd73c5552a
RaW <akram.iiuc.ctg@gmail.com>
2023-09-12 15:22:32 +0600

    Contact Dynamic Message Send and get implemented

--- src/Components/home/Contact/Contact.jsx
+++ src/Components/home/Contact/Contact.jsx
@@ -3,6 +3,8 @@ import Lottie from "lottie-react";
 import { useForm } from "react-hook-form";
 import contactUs from '../../../assets/LottieAnimation/contactUs'
 import { BsSend } from 'react-icons/bs';
+import { toast } from "react-toastify";
+
 const Contact = () => {
   const {
     register,
@@ -11,14 +13,74 @@ const Contact = () => {
     formState: { errors },
   } = useForm();
 
-  const contactDetail = async (data) => {
-    console.log(data);
+  const currentDateLocal = new Date();
+  const timeOffset = 6 * 60 * 60 * 1000;
+  const currentDateBD = new Date(currentDateLocal.getTime() + timeOffset);
+
+  const onSubmitContact = async (data) => {
+    const { name,email,description } = data;
+    const newContact = {
+      name,
+      email,
+      description,
+      createdAt: currentDateBD.toISOString(),
+      updatedAt: currentDateBD.toISOString(),
+    };
+
+    try {
+      const result = await fetch("http://localhost:3000/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newContact),
+      });
+      console.log(result)
+      if (result.ok) {
+        const responseData = await result.json();
+        console.log("Message added:", responseData);
+        toast.success("Message Send!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        reset();
+      } else {
+        toast.error("Failed to Send Message.!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      toast.error("An error occurred!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
   };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 shadow-2xl mb-32 border-t-8 border-[#0083db]">
       <div className="p-10">
         <h1 className="text-4xl font-semibold text-[#0083db] mb-7">Contact us</h1>
-        <form onSubmit={handleSubmit(contactDetail)}>
+        <form onSubmit={handleSubmit(onSubmitContact)}>
           <div className="space-y-6">
             <input
               {...register("name")}
