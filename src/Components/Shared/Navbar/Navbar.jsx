5238735da8c63da0163a256bed26e0d083593793
Akil Tajwar <akiltajwar@gmail.com>
2023-09-13 19:29:49 +0600

    login and signup ui upgrade

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -6,123 +6,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogOut from "@/Components/auth/LogOut";
 import Themes from "../Themes/Themes";
-
+import NavMenu from "./NavMenu";
 
 const Navbar = async () => {
 	const session = await getServerSession(authOptions)
 	console.log(session, 'user session from navbar')
-	const menu = (
-		<>
-			<div className="lg:flex items-center lg:text-xl">
-				<li>
-					<Link href="/">Home</Link>
-				</li>
-				<li>
-					<Link href="/courses">Courses</Link>
-				</li>
-				<li>
-					<Link href="/blogs">Blogs</Link>
-				</li>
-				{/* {session && <li>
-					<Link href="/dashboard">Dashboard</Link>
-				</li>} */}
-				{session && <li>
-					<Link href="/admindashboard">Dashboard</Link>
-				</li>}
-				{session && <li>
-					<Link href="/chat">Chat</Link>
-				</li>}
-			</div>
-		</>
-	);
 	return (
-		<div className="fixed w-full p-color z-50 shadow-sm">
-			<div className="navbar lg:w-3/4 w-11/12 mx-auto">
-				<div className="navbar-start">
-					<div className="dropdown">
-						<label tabIndex={0} className="btn btn-ghost lg:hidden">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M4 6h16M4 12h8m-8 6h16"
-								/>
-							</svg>
-						</label>
-						<ul
-							tabIndex={0}
-							className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-						>
-							{menu}
-						</ul>
-					</div>
-					<a className="btn btn-ghost normal-case text-xl md:text-4xl text-[#0083db]">
-						ED_NEXUS
-					</a>
-				</div>
-				<div className="navbar-center hidden lg:flex">
-					<ul className="menu menu-horizontal px-1 mt-3">{menu}</ul>
-				</div>
-				<div className="navbar-end">
-					<Themes />
-					{session ? <div className="dropdown dropdown-end">
-						<label
-							tabIndex={0}
-							className="btn btn-ghost btn-circle avatar bg-[#0083db] mr-3"
-						>
-							<div className="w-10 rounded-full">
-								<FaUserGraduate className="flex justify-center items-center text-white text-xl ml-[10px] mt-[10px]" />
-							</div>
-						</label>
-						<ul
-							tabIndex={0}
-							className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-						>
-							<li>
-								<div>
-									<div className="flex items-center gap-2">
-										<CgProfile className="text-lg" />
-										<Link href="/profile" className="justify-between">Profile</Link>
-									</div>
-									<div className="badge bg-[#0083db] text-white">New</div>
-								</div>
-							</li>
-							<li>
-								<div>
-									<div className="flex items-center gap-2">
-										<FiSettings className="text-lg" />
-										<Link href="/settings" className="justify-between">Settings</Link>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div>
-									<div className="flex items-center gap-2">
-
-										<FiLogOut className="text-lg" />
-										<LogOut />
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-						:
-						<div className="">
-							<Link href="/login">
-								<button className="text-white lg:text-xl bg-[#0083db] w-24 py-1 border-2 border-[#0083db] rounded-md font-semibold">
-									Login
-								</button>
-							</Link>
-						</div>}
-				</div>
-			</div>
+		<div className="fixed w-full p-color z-50">
+			<NavMenu session={session} />
 		</div>
 	);
 };
--- src/Components/auth/SignIn.jsx
+++ src/Components/auth/SignIn.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaKey, FaMailBulk } from "react-icons/fa";
 import Lottie from "lottie-react";
-import eating from "../../assets/LottieAnimation/education.json";
+import login from "../../assets/LottieAnimation/login-lottie.json";
 import SocialLogin from "./SocialLogin";
 import { useForm } from "react-hook-form";
 import { signIn } from "next-auth/react";
@@ -33,91 +33,77 @@ const SignIn = ({ callbackUrl }) => {
 	};
 
 	return (
-		<div className="relative">
-			<img
-				src="https://i.ibb.co/KVX8GhJ/3659672.jpg"
-				className="absolute inset-0 object-cover w-full h-full"
-				alt=""
-			/>
-			<div className="relative bg-gray-900 bg-opacity-75 ">
-				<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-					<div className="flex flex-col-reverse items-center justify-between lg:flex-row ">
-						<div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-							{<Lottie animationData={eating} loop={true}></Lottie>}
-						</div>
-						<div className="w-full max-w-xl xl:px-8 lg:w-5/12">
-							<div className="bg-[#0083db] rounded shadow-2xl p-7 sm:p-10">
-								<h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl text-white">
-									Login
-								</h3>
-								<form onSubmit={handleSubmit(handleLoginUser)}>
-									<div className="mb-1 sm:mb-2">
-										<div className="relative  mb-3">
-											<FaMailBulk className="absolute left-4 top-[30%] text-black "></FaMailBulk>
-											<input
-												{...register("email", {
-													required: "Email is required",
-													pattern: {
-														value: /\S+@\S+\.\S+/,
-														message: "Email is not valid!",
-													},
-												})}
-												placeholder="Type Your Email"
-												type="text"
-												className="py-3 w-full bg-white border border-gray-300 rounded-full shadow-sm flex-grow px-4  transition duration-200 placeholder:text-black font-medium  outline-none pl-12 bg-none"
-											/>
-											{errors?.email && (
-												<p className="text-red-600">{errors?.email.message}</p>
-											)}
-										</div>
-										<div className="relative mb-3">
-											<FaKey className="absolute left-4 top-[30%] text-black "></FaKey>
-											<input
-												{...register("password", {
-													required: "Password is required!",
-													pattern: {
-														value: /(?=.*[!@#$&*])/,
-														message:
-															"password should be minimum one special character",
-													},
-													minLength: {
-														value: 6,
-														message: "password should be must 6 characters",
-													},
-												})}
-												placeholder="password"
-												type="password"
-												className="py-3 w-full bg-white border border-gray-300 rounded-full shadow-sm flex-grow px-4  transition duration-200 placeholder:text-black font-medium  outline-none pl-12 bg-none"
-											/>
-											{errors?.password && (
-												<p className="text-red-600">
-													{errors?.password.message}
-												</p>
-											)}
-										</div>
-									</div>
-									<div className="mt-4 mb-2 sm:mb-4">
-										<button
-											type="submit"
-											className="inline-flex items-center justify-center w-full py-3 px-6 font-semibold tracking-wide text-white transition duration-200 rounded-full shadow-md outline-none bg-black hover:bg-gray-700 "
-										>
-											{load ? (
-												<span className="border-2 border-dashed border-white animate-spin w-7 h-7 rounded-full"></span>
-											) : (
-												"Login"
-											)}
-										</button>
-									</div>
-								</form>
-								<div>
-									<SocialLogin callbackUrl={callbackUrl}></SocialLogin>
-									<p className="text-xs text-gray-600 sm:text-sm mt-5 cursor-pointer">
-										Create an account?
-										<Link href="/signUp" className="font-semibold">
-											Register
-										</Link>
-									</p>
+		<div className="mx-auto pt-44 pb-32 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24">
+			<div className="flex flex-col-reverse items-center justify-between lg:flex-row ">
+				<div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+					{<Lottie animationData={login} loop={true}></Lottie>}
+				</div>
+				<div className="w-full max-w-xl xl:px-8 lg:w-5/12">
+					<div className="bg-[#0083db] rounded-lg p-7 sm:p-10">
+						<h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl text-white">
+							Login
+						</h3>
+						<form onSubmit={handleSubmit(handleLoginUser)}>
+							<div className="mb-1 sm:mb-2">
+								<div className="relative  mb-3">
+									<FaMailBulk className="absolute left-4 top-[30%] text-black "></FaMailBulk>
+									<input
+										{...register("email", {
+											required: "Email is required",
+											pattern: {
+												value: /\S+@\S+\.\S+/,
+												message: "Email is not valid!",
+											},
+										})}
+										placeholder="Type Your Email" type="text" className="py-3 w-full bg-white border border-gray-300 rounded-full shadow-sm flex-grow px-4  transition duration-200 placeholder:text-black font-medium  outline-none pl-12 bg-none"
+									/>
+									{errors?.email && (
+										<p className="text-red-600">{errors?.email.message}</p>
+									)}
 								</div>
+								<div className="relative mb-3">
+									<FaKey className="absolute left-4 top-[30%] text-black "></FaKey>
+									<input
+										{...register("password", {
+											required: "Password is required!",
+											pattern: {
+												value: /(?=.*[!@#$&*])/,
+												message:
+													"password should be minimum one special character",
+											},
+											minLength: {
+												value: 6,
+												message: "password should be must 6 characters",
+											},
+										})}
+										placeholder="password"
+										type="password"
+										className="py-3 w-full bg-white border border-gray-300 rounded-full shadow-sm flex-grow px-4  transition duration-200 placeholder:text-black font-medium  outline-none pl-12 bg-none"
+									/>
+									{errors?.password && (
+										<p className="text-red-600">
+											{errors?.password.message}
+										</p>
+									)}
+								</div>
+							</div>
+							<div className="mt-4 mb-2 sm:mb-4">
+								<button
+									type="submit"
+									className="inline-flex items-center justify-center w-full py-3 px-6 font-semibold tracking-wide text-white transition duration-200 rounded-full shadow-md outline-none bg-black"
+								>
+									{load ? (
+										<span className="border-2 border-dashed border-white animate-spin w-7 h-7 rounded-full"></span>
+									) : (
+										"Login"
+									)}
+								</button>
+							</div>
+						</form>
+						<div>
+							<SocialLogin callbackUrl={callbackUrl}></SocialLogin>
+							<div className="text-xs gap-1 text-center text-black sm:text-sm mt-5 cursor-pointer">
+								Create an account? <Link href="/signUp" className="font-semibold"> Register</Link>
 							</div>
 						</div>
 					</div>
